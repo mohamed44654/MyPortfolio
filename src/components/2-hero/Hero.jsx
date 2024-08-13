1a9baf8354741928ed43d4305189fd1422b47cac
mohamed khaled <69115922+mohamed44654@users.noreply.github.com>
2024-08-13 04:06:12 +0300

    Update Hero.jsx

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -13,8 +13,8 @@ export default function Hero() {
           <div className='icon-verified'></div>
           </div>
           <h1 className='title'>FRONT-END DEVELOPER</h1>
-          <p className='subTitle'>I am <i>Nouran Mahmoud</i> , graduated from the Faculty of Science, Alexandria University, Computer Science Department.
-            I am Frontend Developer with proficient knowledge in HTML , CSS and Javascript , as well as third-party libraries such as jQuery, and React js
+          <p className='subTitle'>I am <i>Mohamed Khaled</i> , Graduated from the Information Systems Department, CIC (Canadian International College).
+            I am Frontend Developer with proficient knowledge in HTML , CSS and Javascript , as well as third-party libraries such as jQuery, and Angular
            <span>
            <a target='_blank' href="https://drive.google.com/file/d/1tDhMU6kdPLnUuhcLKIU0MEUE6kC-QhlC/view?usp=sharing">view my cv</a>
            </span>
@@ -22,16 +22,10 @@ export default function Hero() {
            
           <div className="AllIcons flex">
             <div>
-              <a className="icon icon-instagram" target='_blank' href="https://www.instagram.com/nouran.kaamal/"></a>
+            <a className="icon icon-github" target='_blank' href="https://github.com/mohamed44654"></a>
             </div>
             <div>
-            <a className="icon icon-twitter" target='_blank' href="https://x.com/Nourankaamal/"></a>
-            </div>
-            <div>
-            <a className="icon icon-github" target='_blank' href="https://github.com/NouranKamal"></a>
-            </div>
-            <div>
-            <a className="icon icon-linkedin" target='_blank' href="https://www.linkedin.com/in/nouran-kamal-b0bb53263/"></a>
+            <a className="icon icon-linkedin" target='_blank' href="https://www.linkedin.com/in/mohamed-khaled-96b006267"></a>
             </div>
           </div>
 
